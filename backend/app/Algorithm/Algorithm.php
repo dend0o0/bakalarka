81471d4f92a6f3a0e15866cb7145893dcadf34e8
Denis Želiezka <dzeliezka@gmail.com>
2025-03-23 19:27:36 +0100

    Pridané komentáre k algoritmu

--- backend/app/Algorithm/Algorithm.php
+++ backend/app/Algorithm/Algorithm.php
@@ -13,6 +13,7 @@ class Algorithm
 
     public function __construct($items)
     {
+        // Nacitanie pouzivanych zoznamov podla toho, kolko ma pouzivatel vytvorenych zoznamov
         $this->items = $items;
         $shop_id = $items->shop_id;
         $this->shoppingLists = ShoppingList::with('items.item')
@@ -36,18 +37,10 @@ class Algorithm
     public function sort()
     {
         $items = $this->items;
-        $pocitadlo = 0;
-        $porovnania = array();
-
-
-
-
         $historicalOrders = [];
         $categoryOrders = [];
 
-        //$items->items = $items->items->filter(fn($item) => $item->checked == 0)->values();
-
-
+        // Ulozenie zoznamov do struktury (cez item sa vieme dostat na zoznamy a cez zoznam na poradie ([item][list])
         foreach ($this->shoppingLists as $list) {
             foreach ($list->items as $item) {
                 $historicalOrders[$item->item_id][$list->id] = $item->order;
@@ -55,26 +48,21 @@ class Algorithm
             }
         }
 
-
+        // Vykonanie bubblesort algoritmu
         for ($i = 0; $i < count($items->items); $i++) {
             $swapped = false;
             for ($j = 0; $j < count($items->items) - $i - 1; $j++) {
-                array_push($porovnania, "Porovnavam " . $items->items[$j]->item->name . " s " . $items->items[$j + 1]->item->name);
                 if ($this->compare($items->items[$j], $items->items[$j + 1], $historicalOrders, $categoryOrders)) {
-                    array_push($porovnania, "\tVymienam " . $items->items[$j]->item->name . " s " . $items->items[$j + 1]->item->name);
                     $temp = $items->items[$j];
                     $items->items[$j] = $items->items[$j + 1];
                     $items->items[$j + 1] = $temp;
                     $swapped = true;
-                    $pocitadlo++;
                 }
             }
             if (!$swapped) {
                 break;
             }
         }
-
-       // dd($porovnania);
         return $items;
     }
 
@@ -107,11 +95,7 @@ class Algorithm
             return $result;
         }
 
-
-
-
         return true;
-
     }
 
     private function compareOrders($id1, $id2, $orders)
@@ -131,8 +115,7 @@ class Algorithm
                     }
                 }
             }
-            //dd($countItem1Before, $countItem2Before, $item1->item_id, $item2->item_id);
-            //dd($countItem1Before, $countItem2Before, $item1, $item2);
+
             if ($countItem1Before != 0 || $countItem2Before != 0) {
 
                     return $countItem1Before >= $countItem2Before;
@@ -152,29 +135,25 @@ class Algorithm
         $neighbors2 = [];
 
         if (!empty($orders[$id1]) && !empty($orders[$id2])) {
-
+            // Hladaju sa spolocne itemy pre prvy item
             foreach ($orders[$id1] as $listId => $order1) {
-
                 foreach ($orders as $itemId => $otherOrders) {
-
-                    if (isset($otherOrders[$listId]) && $otherOrders[$listId] != $order1 /* && ($otherOrders[$listId] == $order1 - 1 || $otherOrders[$listId] == $order1 + 1)*/) {
+                    if (isset($otherOrders[$listId]) && $otherOrders[$listId] != $order1) {
                         $neighbors1[] = $itemId;
                     }
                 }
             }
 
-
+            // Hladaju sa spolocne itemy pre druhy item
             foreach ($orders[$id2] as $listId => $order2) {
-
                 foreach ($orders as $itemId => $otherOrders) {
-
-                    if (isset($otherOrders[$listId]) && $otherOrders[$listId] != $order2 /*&& ($otherOrders[$listId] == $order2 - 1 || $otherOrders[$listId] == $order2 + 1)*/) {
+                    if (isset($otherOrders[$listId]) && $otherOrders[$listId] != $order2) {
                         $neighbors2[] = $itemId;
                     }
                 }
             }
 
-
+            // Prechadzame susedov a zistujeme, ci je nejaky sused spolocny
             foreach ($neighbors1 as $neighbor1) {
                 if (isset($orders[$neighbor1])) {
 
@@ -186,8 +165,8 @@ class Algorithm
                             $greater2 = false;
                             $smaller2 = false;
 
+                            // Porovnavaju sa predmety relativne na zaklade nejakeho predmetu
                             foreach ($orders[$neighbor1] as $listId => $order1) {
-                                //dd($historicalOrders[$item1->item_id], $historicalOrders[$item2->item_id], $historicalOrders[$neighbor1], $historicalOrders[$neighbor2], $neighbor1, $neighbor2);
 
                                 if (isset($orders[$neighbor1][$listId]) && isset($orders[$id1][$listId])) {
                                     if ($orders[$neighbor1][$listId] > $orders[$id1][$listId]) {
@@ -216,14 +195,7 @@ class Algorithm
                 }
 
             }
-
-            //dd($item1->item_id, $item2->item_id, $neighbors1, $neighbors2);
-
         }
-        //dd($neighbors1, $neighbors2);
         return null;
-
-
     }
-
 }
